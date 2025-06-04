348622dba49099e75890655a7cf86e91c138231c
harshi-31 <harshitelikepalli@gmail.com>
2025-06-04 14:58:43 +0530

    Update

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,4 @@
 
-
-// App.js
-/*import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UploadForm from './components/UploadForm';
-import ReportList from './components/ReportList';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UploadForm />} />
-        <Route path="/reports" element={<ReportList />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;*/
-
-
 import React from 'react';
 import UploadForm from './components/UploadForm';
 import ReportList from './components/ReportList';
